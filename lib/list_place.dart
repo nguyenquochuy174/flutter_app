04041454dd575e8471c33a6cc279358a8ec8884b
nguyenquochuy174 <huymonkey6403@gmail.com>
2025-12-14 20:30:41 +0700

    Update

--- lib/list_place.dart
+++ lib/list_place.dart
@@ -3,13 +3,16 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ListPlace extends StatelessWidget {
-  const ListPlace ({super.key});
+  const ListPlace({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Danh sách địa điểm", style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Danh sách địa điểm",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
@@ -21,80 +24,91 @@ class ListPlace extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 header(),
-                SizedBox(height: 30,),
+                SizedBox(height: 30),
                 RichText(
                   text: TextSpan(
                     text: 'Welcome, ',
-                    style: TextStyle( fontSize: 60, fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                     children: <TextSpan>[
                       TextSpan(
-                          text: 'Charlie',
-                          style: TextStyle(fontWeight: FontWeight.normal)),
-                      
+                        text: 'Charlie',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                      ),
                     ],
                   ),
                 ),
                 search(),
-                SizedBox(height: 80,),
-                Text("Saved Places", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                SizedBox(height: 10,),
+                SizedBox(height: 80),
+                Text(
+                  "Saved Places",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
                 listPlace(),
               ],
             ),
-          )
+          ),
         ),
       ),
     );
   }
+
   Widget header() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-       IconButton(onPressed: (){
-          print("Ok rồi đấy 1!!!!");
-        }, icon: Icon(Icons.notifications, color: Colors.black,),),
-        IconButton(onPressed: (){
-          print("Ok rồi đấy 2!!!!");
-        }, icon: Icon(Icons.extension, color: Colors.black,),),
+        IconButton(
+          onPressed: () {
+            print("Ok rồi đấy 1!!!!");
+          },
+          icon: Icon(Icons.notifications, color: Colors.black),
+        ),
+        IconButton(
+          onPressed: () {
+            print("Ok rồi đấy 2!!!!");
+          },
+          icon: Icon(Icons.extension, color: Colors.black),
+        ),
       ],
     );
   }
+
   Widget search() {
     return Padding(
       padding: const EdgeInsets.only(top: 20),
       child: TextField(
         decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search, color: Colors.blue,), 
+          prefixIcon: Icon(Icons.search, color: Colors.blue),
           hintText: 'Search',
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Colors.blue,  
-              width: 2,
-            ),
+            borderSide: BorderSide(color: Colors.blue, width: 2),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Colors.blue,  
-              width: 2,
-            ),
+            borderSide: BorderSide(color: Colors.blue, width: 2),
           ),
         ),
       ),
     );
   }
+
   Widget placeItem(String imgPath) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        image: DecorationImage(
-          image: AssetImage(imgPath),
-          fit: BoxFit.cover,
-        ),
+        image: DecorationImage(image: AssetImage(imgPath), fit: BoxFit.cover),
       ),
     );
   }
+
   Widget listPlace() {
     final Random random = Random();
     final images = [
@@ -103,15 +117,16 @@ class ListPlace extends StatelessWidget {
       "assets/images/place3.jpg",
       "assets/images/place4.jpg",
     ];
+
     return GridView.count(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       crossAxisCount: 2,
       crossAxisSpacing: 20,
       mainAxisSpacing: 20,
       children: List.generate(15, (index) {
-        return placeItem( images[random.nextInt(images.length)]);
+        return placeItem(images[random.nextInt(images.length)]);
       }),
     );
   }
-
 }
